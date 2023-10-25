bac210f6c73de10d2850af0eb12db1a5c085b365
Hiromichi129 <aska129424@gmail.com>
2023-10-25 18:15:12 +0900

    Update english.js

--- diagnostic_test/english.js
+++ diagnostic_test/english.js
@@ -1,9 +1,9 @@
 const toLogin = () => {
-    window.location.href = "/test/login_form.php"
+    window.location.href = "/diagnostic_test/login_form.php"
 }
 
 const toMypage = () => {
-    window.location.href = "/test/mypage.php"
+    window.location.href = "/diagnostic_test/mypage.php"
 }
 
 const element = document.getElementsByClassName("Question_E")[0];
